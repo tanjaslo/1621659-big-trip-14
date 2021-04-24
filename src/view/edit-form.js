b166f02ecdbec7c8657c3f2312ade7d9c4721624
tanjaslo <tanja.slo@ukr.net>
2021-04-24 21:03:40 +0200

    Обучает компонент обновлять шаблон и данные.

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -140,6 +140,30 @@ export default class EditForm extends SmartView {
     this.setFormSubmitHandler(this._callback.formSubmit);
   }
 
+  updateData(update) {
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign(
+      {},
+      this._data,
+      update,
+    );
+
+    this.updateElement();
+  }
+
+  updateElement() {
+    const prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, prevElement);
+  }
+
   _editFormSubmitHandler(evt) {
     evt.preventDefault();
     this._callbacks.formSubmit(EditForm.parseStateToPoint(this._data));
