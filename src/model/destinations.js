629b9102a054af63d800a6a2e543e3e3c9a361cb
tanjaslo <tanja.slo@ukr.net>
2021-05-05 21:12:42 +0200

    Исправляет название метода.

--- src/model/destinations.js
+++ src/model/destinations.js
@@ -6,7 +6,7 @@ export default class Destinations extends Observer {
     this._destinations = [];
   }
 
-  setFilter(updateType, destinations) {
+  setDestinations(updateType, destinations) {
     this._destinations = destinations.slice();
     this._notify(updateType);
   }
